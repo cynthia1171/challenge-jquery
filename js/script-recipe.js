8d25e4d5210f7d86352922f941cbdb92c21c797a
cynthia1171 <c.isla1171@gmail.com>
2018-01-08 16:29:55 -0300

    Etapa 10: retroceder a index.html apretando elemento .js-back

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,14 +2,38 @@ $(document).ready(function(){
   console.log('he cargado');
 })
 
+/**
+ * ocultar elemento js-menu mientras este en pagina recipe.html
+ */
+var path = window.location.pathname.split("/");
+var pagina = path[path.length - 1];
+if (pagina === 'recipe.html') {
+  $('.js-menu').hide();
+}
+
+/**
+ * funcion ocultar clase make y añadir clase active
+ */
 $('.js-show-recipe').click(function(){
   $('.page').removeClass('make');
   $('.js-show-recipe').addClass('active')
   $('.js-show-make').removeClass('active');
 })
 
+
+/**
+ * funcion añadir clase make y añadir clase active
+ */
 $('.js-show-make').click(function () {
   $('.page').addClass('make');
   $('.js-show-make').addClass('active');
   $('.js-show-recipe').removeClass('active');
+})
+
+
+/**
+ * funcion para retroceder a index.html dando click al elemento js-back
+ */
+$('.js-back').click(function(){
+  window.location = 'index.html';
 })
