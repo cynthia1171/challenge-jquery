0e0d8ed9b7918db4efa8dc78175bf8345b59ad2b
cynthia1171 <c.isla1171@gmail.com>
2018-01-08 16:17:47 -0300

    Etapa 8: agregar y quitar clase make

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
   console.log('he cargado');
 })
+
+$('.js-show-recipe').click(function(){
+  $('.page').removeClass('make');
+})
+
+$('.js-show-make').click(function () {
+  $('.page').addClass('make');
+})
